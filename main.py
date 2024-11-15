e7eb98ffa3701df3c8974cef2f270958fe49ea06
Leandrosaints <leo320487@gmail.com>
2024-11-15 18:10:51 -0400

    atualizacão simples

--- main.py
+++ main.py
@@ -33,7 +33,6 @@ if __name__ == "__main__":
 
     # Iniciar o servidor Uvicorn
     uvicorn.run('main:app',
-                host='127.0.0.1', port=8000,
                 log_level='info',
                 reload=True, debug=True)
 
